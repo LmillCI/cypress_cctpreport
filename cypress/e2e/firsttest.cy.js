05e6c6a4fc3594da53310cc72a5c2d9cb9255420
LmillCI <168177275+LmillCI@users.noreply.github.com>
2024-08-08 19:02:04 +0530

    Update firsttest.cy.js

--- cypress/e2e/firsttest.cy.js
+++ cypress/e2e/firsttest.cy.js
@@ -15,15 +15,15 @@ describe('Sample suite', () => {
       const  screenshotName = `${suiteName}-${testName}`;
       cy.task('saveScreenshotInfo', {  screenshotName, suiteName, testName, testSteps , fileName });
   });
-it('TestCase1-  verify title-negative test', function() {
-    cy.visit("https://opensource-demo.orangehrmlive.com/");
-    cy.screenshot('orangehrm homepage')
-     addTestStep('visit', ' orangehrm homepage');
-    cy.title().should('eq', 'OrangeHRM123');
-   cy.screenshot('title verification error')
-    addTestStep('Assertion', 'title verification error');
+// it('TestCase1-  verify title-negative test', function() {
+//     cy.visit("https://opensource-demo.orangehrmlive.com/");
+//     cy.screenshot('orangehrm homepage')
+//      addTestStep('visit', ' orangehrm homepage');
+//     cy.title().should('eq', 'OrangeHRM123');
+//    cy.screenshot('title verification error')
+//     addTestStep('Assertion', 'title verification error');
  
-  });
+//   });
   it('TestCase2 - Saucedemo Site', function() {
     cy.visit("https://www.saucedemo.com/v1/").screenshot('saucedemo');
     addTestStep('visit website', 'saucedemo')
